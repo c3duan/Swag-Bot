78750b254c6c1a3074239ea099d4eca749304e46
c3duan <c3duan@ucsd.edu>
2018-07-17 11:36:11 +0800

    Small Features with if-else

--- index.js
+++ index.js
@@ -16,20 +16,63 @@ client.on('ready', () => {
 client.login(token);
 
 client.on('message', message => {
-    if (message.content.startsWith(`${prefix}ping`)) {
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+    else if (command === 'ping') {
         // send back "Pong." to the channel the message was sent in
         message.channel.send('Pong.');
     }
-    else if (message.content.startsWith(`${prefix}beep`)) {
+    else if (command === 'beep') {
         message.channel.send('Boop.');
     }
-    else if (message.content == `${prefix}server`) {
+    else if (command === 'server') {
         message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     }
-    else if (message.content == `${prefix}user-info`) {
+    else if (command === 'user-info') {
         message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
     }
-    else if (message.content == `Logan`) {
-        message.channel.send('Logan the degenerate who does not have a hairline, rip losupo');
+    else if (command === 'Logan') {
+        message.channel.send('Logan (aka the degenerate boy) does not have a hairline, rip losupo');
     }
+    else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+        else if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+        message.channel.send(`First Argument: ${args[0]}`);
+    }
+    else if (command === 'kick' ) {
+        if (!message.mentions.users.size) {
+            return message.reply('You need to tag a user in order to kick them!');
+        }
+        else {
+            // grab the "first" mentioned user from the message
+            // this will return a `User` object, just like `message.author`
+            const taggedUser = message.mentions.users.first();
+
+            message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+        }
+    }
+
+    else if (command == 'avatar') {
+        if (!message.mentions.users.size) {
+            return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
+        }
+
+        const avatarList = message.mentions.users.map(user => {
+            return `${user.username}'s avatar: ${user.displayAvatarURL}`;
+        });
+
+        // send the entire array of strings as a message
+        // by default, discord.js will `.join()` the array with `\n`
+        message.channel.send(avatarList);
+    }
+    
 });
